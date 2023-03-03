32ec157100a2cc08ef98c2c99130579cdeb8f770
AndriiTkac4ov <106906924+AndriiTkac4ov@users.noreply.github.com>
2023-03-03 22:07:58 +0200

    start step 2

---
+++ contacts.js
@@ -0,0 +1,26 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const contactsPath = path.resolve('db/contacts.json');
+
+function listContacts() {
+    fs.readFile(contactsPath, 'utf8', (error, data) => {
+        if (error) {
+            console.error(error);
+        }
+        
+        console.log(data);
+    })
+}
+
+function getContactById(contactId) {
+  // ...твій код
+}
+
+function removeContact(contactId) {
+  // ...твій код
+}
+
+function addContact(name, email, phone) {
+  // ...твій код
+}
