450c61fb09961f870f0860c5c52afa7f97e7ef93
AndriiTkac4ov <106906924+AndriiTkac4ov@users.noreply.github.com>
2023-03-05 21:13:30 +0200

    do step 3

--- contacts.js
+++ contacts.js
@@ -1,28 +1,91 @@
 const fs = require('fs').promises;
 const path = require('path');
+const colors = require('colors/safe');
 
 const contactsPath = path.resolve('db/contacts.json');
 
-function listContacts() {
-    fs.readFile(contactsPath, 'utf8', (error, data) => {
-        if (error) {
-            console.error(error);
-        }
-        
-        console.log(data);
-    })
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, 'utf8');
+    const listContacts = JSON.parse(data);
+  
+    // console.table(listContacts);
+
+    return listContacts;
+  } catch (error) {
+    console.log(colors.brightRed(error.name));
+    console.log(colors.brightRed(error.message));
+  }
 }
 
-function getContactById(contactId) {
-  // ...твій код
+async function getContactById(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, 'utf8');
+    const contactById = JSON.parse(data).find(contact => contact.id === String(contactId));
+    if (!contactById) {
+      console.log(colors.brightMagenta('Sorry, but this contact does not exist.'));
+      return;
+    }
+
+    // console.log(colors.brightYellow(contactById));
+
+    return contactById;
+  } catch (error) {
+    console.log(colors.brightRed(error.name));
+    console.log(colors.brightRed(error.message));
+  }
 }
 
-function removeContact(contactId) {
-  // ...твій код
+async function removeContact(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, 'utf8');
+    const contactById = JSON.parse(data).find(contact => contact.id === String(contactId));
+    if (!contactById) {
+        console.log(colors.brightMagenta('Sorry, but this contact does not exist.'));
+        return;
+    };
+    
+    const newContactsList = JSON.parse(data).filter(contact => contact.id !== String(contactId));
+    await fs.writeFile(contactsPath, JSON.stringify(newContactsList), 'utf8');
+
+    console.log(colors.brightGreen('The contact was removed successful.'));
+    return;
+  } catch (error) {
+    console.log(colors.brightRed(error.name));
+    console.log(colors.brightRed(error.message));
+  }
 }
 
-function addContact(name, email, phone) {
-  // ...твій код
+async function addContact(name, email, phone) {
+  try {
+    const data = await fs.readFile(contactsPath, 'utf8');
+    const listContacts = JSON.parse(data);
+
+    const newContact = {
+      id: String(listContacts.length + 1),
+      name,
+      email,
+      phone,
+    };
+
+    const contactByName = JSON.parse(data).find(contact => contact.name === name);
+    const contactByEmail = JSON.parse(data).find(contact => contact.email === email);
+    const contactByPhone = JSON.parse(data).find(contact => contact.phone === phone);
+
+    if (contactByName && contactByEmail && contactByPhone) {
+        console.log(colors.brightMagenta('Attention, this contact has already existed.'));
+        return;
+    };
+    
+    listContacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(listContacts), 'utf8');
+
+    console.log(colors.brightGreen('The new contact was added successful.'));
+    return;
+  } catch (error) {
+    console.log(colors.brightRed(error.name));
+    console.log(colors.brightRed(error.message));
+  }
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const {
     addContact,
 } = require('./contacts');
 
-listContacts();
+// listContacts();
+// getContactById(17);
+// removeContact(20);
+// addContact('Alina Tkachova', 'alina@ukr.net', '093-786-80-82');
+// addContact('Andrii Tkach', 'andrii@ukr.net', '066-786-80-84');
 
-console.log(process.argv);
+// console.log(process.argv);
